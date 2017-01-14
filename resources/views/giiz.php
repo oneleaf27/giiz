19b45202a4ef2bd0ac0fda4c1098d163c7752ac0
oneleaf27 <skbnleaf@gmail.com>
2017-01-15 01:29:09 +0900

    軽微な修正

--- resources/views/giiz.php
+++ resources/views/giiz.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>ガルパンはいいぞボタン α版</title>
-        <meta name="Description" content="" />
-        <meta name="Keywords"  content="" />
+        <title>ガルパンはいいぞセンター α版</title>
+        <meta name="Description" content="「ガルパンはいいぞ」をみんなで共有できるWebサービスであります！" />
+        <meta name="Keywords"  content="ガルパンはいいぞ, いいぞ, ガルパン, 共有, " />
 
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
@@ -17,6 +17,17 @@
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
         <script type="text/javascript" src="js/giiz.js"></script>
+        <script type="text/javascript" src="js/googleAnalytics.js"></script>
+        <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69814974-7', 'auto');
+  ga('send', 'pageview');
+
+</script>
     </head>
     <body>
       <div id="contents">
