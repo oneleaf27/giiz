b892ded705f0f9af79d30e13f694f30ab1676e7e
oneleaf27 <skbnleaf@gmail.com>
2017-01-17 00:40:13 +0900

    軽微な修正

--- resources/views/iiz.php
+++ resources/views/iiz.php
@@ -5,18 +5,18 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>ガルパンはいいぞセンター</title>
+        <title>ガルパンはいいぞカウンター</title>
         <meta name="Description" content="「ガルパンはいいぞ」をみんなで共有できるWebサービスであります！" />
-        <meta name="Keywords"  content="ガルパンはいいぞ, いいぞ, ガルパン, 共有" />
+        <meta name="Keywords"  content="ガルパンはいいぞ", "いいぞ", "ガルパン", "共有", "ボタン", "カウンター"/>
 
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="css/giiz.css" />
+        <link rel="stylesheet" type="text/css" href="css/iiz.css" />
 
         <!-- JavaScript -->
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
-        <script type="text/javascript" src="js/giiz.js"></script>
+        <script type="text/javascript" src="js/iiz.js"></script>
         <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -28,16 +28,12 @@
         </script>
     </head>
     <body>
-      <div id="contents">
-        <div class="content">
-          <div id="head">
-            <h1>「ガルパンはいいぞ」<br>をみんなで共有<br>できるであります！</h1>
-          </div>
-          <button id="add" type="button" class="btn btn-success btn-lg active">いいぞ</button>
-          <div id="counter"></div>
-          <div>
-            <ul id="onecount"></ul>
-          </div>
+      <div id="contents" class="contents">
+        <h1>「ガルパンはいいぞ」<br>をみんなで共有して<br>集めるであります！</h1>
+        <button id="add" type="button" class="btn btn-success btn-lg active">いいぞ</button>
+        <div id="counter"></div>
+        <div>
+          <ul id="onecount"></ul>
         </div>
       </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('giiz');
+    return view('iiz');
 });
 
 
